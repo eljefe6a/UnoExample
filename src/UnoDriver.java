66b081f56ec518831882291bb5f0d48b3c97e534
Jesse Anderson <eljefe6a@gmail.com>
2013-03-19 19:31:35 -0400

    Fixed writable type.

--- src/UnoDriver.java
+++ src/UnoDriver.java
@@ -1,6 +1,7 @@
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
@@ -35,7 +36,7 @@ public class UnoDriver extends Configured implements Tool {
 		job.setMapOutputValueClass(DoubleWritable.class);
 
 		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(DoubleWritable.class);
+		job.setOutputValueClass(IntWritable.class);
 
 		boolean success = job.waitForCompletion(true);
 		return success ? 0 : 1;
